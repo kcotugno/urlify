948f2672cce454570857b96ba9cc14ded6fe44ec
Kevin Cotugno <kevin@kevincotugno.com>
2018-03-21 20:33:14 -0700

    Fix typo

--- src/main.rs
+++ src/main.rs
@@ -21,7 +21,7 @@ fn parse_args() -> String {
     let mut args: Vec<String> = env::args().collect();
 
     if args.len() < 2 {
-        panic!("Must supply an string to encode");
+        panic!("Must supply a string to encode");
     }
 
     args.remove(1)
